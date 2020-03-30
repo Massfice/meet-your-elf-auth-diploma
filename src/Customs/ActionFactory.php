958361feb07fb607c618336f033ad50777fbf137
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2020-03-30 09:44:31 +0200

    Test

--- src/Customs/ActionFactory.php
+++ src/Customs/ActionFactory.php
@@ -17,7 +17,7 @@ abstract class ActionFactory {
     }
 
     public function create(string $name) : JsonAction {
-        $action = $this->namespace.ucfirst($name);
+        $action = $this->namespace.$name;
         if($this->check($name)) {
             return new $action();
         } else {
