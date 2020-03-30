67bb1031b922629f764c22005f88518b623023de
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2020-03-30 09:38:00 +0200

    Test

--- src/Customs/ActionFactory.php
+++ src/Customs/ActionFactory.php
@@ -26,7 +26,19 @@ abstract class ActionFactory {
     }
 
     public function check(string $name) : bool {
+        echo "--------------------------------------<br>";
         $action = $this->namespace.ucfirst($name);
+        echo "--------------------------------------<br>";
+        var_dump($action);
+        echo "--------------------------------------<br>";
+        var_dump(class_exists($action));
+        echo "--------------------------------------<br>";
+        var_dump(class_implements($action));
+        echo "--------------------------------------<br>";
+        var_dump(isset(class_implements($action)[$this->interface]));
+        echo "--------------------------------------<br>";
+        echo "--------------------------------------<br>";
+
         return class_exists($action) && isset(class_implements($action)[$this->interface]);
     }
 
